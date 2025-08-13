e919b259c9ed7408219eea5afd8a197bb1dc9521
Christian Goerlach <christian.goerlach@web.de>
2025-08-13 08:01:51 +0200

    0801

--- index2.php
+++ index2.php
@@ -1,28 +1,21 @@
 
-0753
+0801
+
 <?php
 header('Content-Type: application/json');
 
-// Verbindung zur Datenbank herstellen
-require_once 'db.php';
+require_once 'db.php'; // DB-Verbindung
 
-// SQL nur die benötigte Spalte holen
 $sql = "SELECT [FileName] FROM [dbo].[ImageRegistry]";
 $stmt = $conn->query($sql);
 
-// Array für URLs vorbereiten
 $imageUrls = [];
-
-// Azure Blob Storage Basis-URL (bitte anpassen)
-$blobBaseUrl = "https://cgml1519944712.blob.core.windows.net/adntest/";
+$blobBaseUrl = "https://cgml1519944712.blob.core.windows.net/adntest/"; // anpassen falls nötig
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $fileName = $row['FileName'];
-
-    // URL zusammensetzen
     $imageUrls[] = $blobBaseUrl . rawurlencode($fileName);
 }
 
-// JSON ausgeben
-echo json_encode($imageUrls);
+echo json_encode($imageUrls, JSON_UNESCAPED_SLASHES);
 ?>
